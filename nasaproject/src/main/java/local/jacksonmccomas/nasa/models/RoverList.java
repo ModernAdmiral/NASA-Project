78a5fd86653db1a5e6519666740b17c13b068e0f
Jackson McComas <jackson.mccomas@gmail.com>
2020-07-21 22:02:49 -0500

    chore: more cleanup and refinement.

--- nasaproject/src/main/java/local/jacksonmccomas/nasa/models/RoverList.java
+++ nasaproject/src/main/java/local/jacksonmccomas/nasa/models/RoverList.java
@@ -5,12 +5,4 @@ import java.util.List;
 public class RoverList {
 
 	public List<Rover> rovers;
-
-	public List<Rover> getRovers() {
-		return rovers;
-	}
-
-	public void setRovers(final List<Rover> rovers) {
-		this.rovers = rovers;
-	}
 }
